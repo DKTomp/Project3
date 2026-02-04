1d921440fc6097ecf3e782a1c1b9067a92bd1979
DKTomp <dylan.tompkins1991@yahoo.com>
2026-02-03 21:52:22 -0600

    update package.json

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Todo />} /> 
         <Route path="/contact" element={<Contact />} />    
