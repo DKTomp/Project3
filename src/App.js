bcef1145df7acc9977cb8732c6dc7318675bd22e
DKTomp <dylan.tompkins1991@yahoo.com>
2026-02-04 17:47:42 -0600

    update hash router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Todo from './Pages/Todo';
 import Contact from './Pages/Contact';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Todo />} /> 
         <Route path="/contact" element={<Contact />} />    
       </Routes>
-    </BrowserRouter> 
+    </HashRouter> 
   );
 }
 
